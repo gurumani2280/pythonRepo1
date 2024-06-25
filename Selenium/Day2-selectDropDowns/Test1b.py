b5bb1f0f7d5ac70b28eda1b1a9cf405e31bd4348
unknown <mani.2280@gmail.com>
2024-06-25 07:33:36 +0530

    alert

---
+++ Selenium/Day2-selectDropDowns/Test1b.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.select import Select
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("file:///D:/Development/repo/pythonRepo1/Selenium/BasicHtmlElement.html")
+time.sleep(3)
+
+statedropdown = driver.find_element(By.NAME,'state')
+sel = Select(statedropdown)
+sel.select_by_index(3)
+time.sleep(1)
+sel.select_by_value("2")
+time.sleep(1)
+sel.select_by_visible_text("KARNATAKA")
+time.sleep(3)
+print(sel.is_multiple)
+
+optionList=sel.options
+#print(optionList)
+print(type(optionList))
+for option in optionList:
+    print(option.text)
+driver.quit()
+
+
+
+
